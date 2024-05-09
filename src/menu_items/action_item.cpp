60438fa9db50cd3a2f0379a9a55043673a54dfb1
APICode06 <blau.fefran270805@gmail.com>
2024-05-09 22:23:03 +0200

    Add action item

---
+++ src/menu_items/action_item.cpp
@@ -0,0 +1,42 @@
+#include "lcdmenu.h"
+
+using namespace lcdgui;
+
+void ActionMenuItem::build(LcdBuffer &buffer, const uint8_t &max_length) {
+  buffer << text_;
+}
+
+bool ActionMenuItem::input(const uint8_t &input, gui_dialog dialog) {
+  if(input == LCD_INPUT_NEXT || input == LCD_INPUT_LEFT) {
+    if(event_ != nullptr) {
+      event_();
+    }
+    return EVENT_CANCELLED;
+  }
+  return EVENT_SEND;
+}
+
+ActionMenuItem::ActionMenuItem(const String &text, action_event_t action_event) {
+  text_ = text;
+  event_ = action_event;
+}
+
+action_event_t ActionMenuItem::event() {
+  return event_;
+}
+
+String ActionMenuItem::text() const {
+  return { text_ };
+}
+
+void ActionMenuItem::text(const String &text) {
+  text_ = text;
+}
+
+menu_item ActionMenuItem::clone() const {
+  return new ActionMenuItem(text_, event_);
+}
+
+ItemType ActionMenuItem::type() const {
+  return ItemType::Action;
+}
